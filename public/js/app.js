2322194d5abe2486d4d5d056a7a1f31b28fe3bba
Teacher <Teacher>
2016-06-21 20:55:13 -0400

    completed user story 4,5,6

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,35 @@
 
+$(document).ready(function(){
+  var start = $('#start');
+  var minutes = $('#minutes');
+  var seconds = $('#seconds');
+  var breakBtn = $('#break');
+
+  start.on('click', startCountdown);
+
+  function startCountdown(){
+    setInterval(function(){
+      var secondsVal = +seconds.text();
+      var minutesVal = +minutes.text();
+     if(secondsVal === 0 && minutesVal === 0){
+       breakBtn.removeClass('disabled');
+       breakBtn.removeAttr('disabled');
+     }
+
+      if(secondsVal === 0){
+        minutes.text(minutesVal -1);
+        seconds.text(59);
+      } else {
+        if(secondsVal <= 10){
+          seconds.text("0" + (secondsVal - 1));
+        } else {
+          seconds.text(secondsVal -1);
+        }
+
+      }
+
+    console.log("meadfkjn!!!")
+
+    }, 1000);
+  }
+})
