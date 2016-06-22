80c5cd6ba43ca82bf9cc87ce9906ed128b16a1c7
Teacher <Teacher>
2016-06-21 21:04:24 -0400

    completed userstory 7

--- public/js/app.js
+++ public/js/app.js
@@ -6,14 +6,23 @@ $(document).ready(function(){
   var breakBtn = $('#break');
 
   start.on('click', startCountdown);
+  breakBtn.on('click', takeABreak);
+
+    function takeABreak(){
+      minutes.text('05');
+      seconds.text('00');
+      startCountdown();
+    }
 
   function startCountdown(){
-    setInterval(function(){
+      var countdown = setInterval(function(){
       var secondsVal = +seconds.text();
       var minutesVal = +minutes.text();
      if(secondsVal === 0 && minutesVal === 0){
        breakBtn.removeClass('disabled');
        breakBtn.removeAttr('disabled');
+       clearInterval(countdown);
+       return;
      }
 
       if(secondsVal === 0){
