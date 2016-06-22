d6893d7771a4f94adec2fe6c7be533d913567e1e
Teacher <Teacher>
2016-06-21 20:38:44 -0400

    finished user story number two

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,31 @@
 
+$(document).ready(function(){
+  var start = $('#start');
+  var minutes = $('#minutes');
+  var seconds = $('#seconds');
+
+  start.on('click', startCountdown);
+
+  function startCountdown(){
+    setInterval(function(){
+      var secondsVal = +seconds.text();
+      var minutesVal = +minutes.text();
+  //    if(secondsVal === 0 && minutesVal === 0)
+
+      if(secondsVal === 0){
+        minutes.text(minutesVal -1);
+        seconds.text(59);
+      } else {
+        if(secondsVal <= 10){
+          seconds.text("0" + (secondsVal - 1));
+        } else {
+          seconds.text(secondsVal -1);
+        }
+
+      }
+
+    console.log("meadfkjn!!!")
+
+    }, 1000);
+  }
+})
